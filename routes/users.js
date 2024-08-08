de35d90484ad726eee3f9aec1c7c20f9bd757db0
Bhumi Gupta <bhumi.gupta@example.com>
2024-08-08 21:54:14 +0530

    com

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,17 @@
+require('dotenv').config();  
+
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/login');
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
-})
-
-
-
+});
 
 module.exports = mongoose.model("User", userSchema);
