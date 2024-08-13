4ff39cb7d3d5c2b9dc93e5c161867b4b54e33acd
Bhumi Gupta <bhumi.gupta@example.com>
2024-08-13 12:19:24 +0530

    user

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,4 @@
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1:27017/login');
+const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
