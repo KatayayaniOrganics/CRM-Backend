b30736aee6d058163fb2c7ea3001cf506f367344
tyrant0407 <amankushwaha1331@gmail.com>
2024-08-20 14:49:50 +0530

    Error solved

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,18 +5,18 @@ exports.verifyToken = async (req, res, next) => {
     console.log(authHeader);
     
     if (!authHeader) {
-      return res.status(403).json({ message: "No token provided" });
+      return res.status(401).json({ message: "No token provided" });
     }
     
     const token = authHeader.split(" ")[1];
     
     if (!token) {
-      return res.status(403).json({ message: "No token provided" });
+      return res.status(401).json({ message: "No token provided" });
     }
     
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
-        return res.status(401).json({ message: "Invalid token" });
+        return res.status(403).json({ message: "Invalid token" });
       }
       req.user = decoded;
       next();
