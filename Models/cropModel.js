bf8f43f4b1931ee1b4b72374c509a7b2a8c23645
Ankit Niranjan <iambot331@gmail.com>
2024-08-30 14:36:45 +0530

    cropid

--- Models/cropModel.js
+++ Models/cropModel.js
@@ -1,11 +1,22 @@
 const mongoose = require("mongoose");
 
 const cropSchema = new mongoose.Schema({
+  cropId:{
+    type:String,
+    default:"CS-01",
+    unique:true,
+  },
   name: { type: String, required: [true, "Name is required"] },
   sowing: Date,
   products_used: [String],
   crop_stage: String,
+  image: String, // Image of the crop
+  diseases: [{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"Disease"
+  }], // Array of diseases related to the crop
 });
+
 
 const Crop = mongoose.model("Crop", cropSchema);
 
