620b8a6d89085620b1a137f3828d676c1af86c9c
Ankit Niranjan <iambot331@gmail.com>
2024-09-10 12:04:34 +0530

    DeleteAndUpdateCrop

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 const { createLead, searchLead ,allLeads, updateLead ,deleteLead,kylasLead } = require('../controllers/LeadControllers');
-const {createSource,createTags,queryCreation,CropsCreation,searchCrop, getAlluserRoles, CreateUserRoles ,updateUserRole}=require('../controllers/indexControllers');
+const {createSource,createTags,queryCreation,CropsCreation,searchCrop, getAlluserRoles, CreateUserRoles ,updateUserRole, updateCrop, deleteCrop}=require('../controllers/indexControllers');
 const { verifyToken } = require('../middlewares/authMiddleware');
 const {CallDetailsCreation, CallUpdate, CallDelete} = require('../controllers/CallController');
 const { checkTokenExpiration } = require('../middlewares/refreshMiddleware');
@@ -44,6 +44,12 @@ router.post('/crops',CropsCreation );
 
 //search crop
 router.get('/searchCrops',verifyToken,searchCrop);
+
+//Update Crop
+router.put("/updateCrop/:cropId",updateCrop);
+
+//Delete Crop
+router.delete("/deleteCrop/:cropId", deleteCrop);
 
 // Create a new source
 router.post('/sources',createSource);
