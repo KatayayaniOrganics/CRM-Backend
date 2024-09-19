163d2dca8a7730d79d0a7fd18eb93895759cd10b
Ankit Niranjan <iambot331@gmail.com>
2024-09-19 18:17:47 +0530

    CropChanges

--- Models/diseaseModel.js
+++ Models/diseaseModel.js
@@ -1,28 +1,41 @@
 const mongoose = require("mongoose");
 
-const diseaseSchema = new mongoose.Schema({
+const diseaseSchema =new mongoose.Schema({
   diseaseId: {
     type: String,
     unique: true,
     default: "DO-1000",
   },
-  name: { type: String, required: true }, 
+  name: { type: String, required: true },
   diseaseImage: {
-    type:String,
-    default:null
+    type: String,
+    default: null,
   },
   solution: [{
-    type:String,
-    default:null
-  }], 
+    type: String,
+    default: null,
+  }],
   prevention: [{
-    type:String,
-    default:null
-  }], 
+    type: String,
+    default: null,
+  }],
   products: [{
-    type:String,
-    default:null
-  }], 
+    type: String,
+    default: null,
+  }],
+  updatedData: [
+    {
+      updatedBy: { type: String, ref: "Agents" }, // Using agentId instead of ObjectId
+      updatedFields: { type: Object },
+      updatedAt: { type: Date, default: Date.now },
+      ipAddress: { type: String },  // New field to store IP address
+      updatedByEmail:{type:String}
+    },
+  ],
+  LastUpdated_By: {
+    type: String, // Use agentId here
+    ref: "Agents", // Reference the Agent schema using agentId
+  },
 });
 
 const Disease = mongoose.model("Disease", diseaseSchema);
