0ee03e0a0af0f25099f45c7a0024017a390f7b8e
tyrant0407 <amankushwaha1331@gmail.com>
2024-09-23 20:26:45 +0530

    updatedleadspagination

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const createError = require('http-errors');
-const logger = require ("./logger");
-const morgan = require ("morgan");
+const logger = require("./logger");
+const morgan = require("morgan");
 const cors = require('cors');
+const helmet = require('helmet'); // Add helmet for security
 
-
+// Connect to the database
 require("./Models/Database.js").connectDatabase();
 
+// Import route handlers
 const leadRouter = require('./routes/Lead-routes');
 const agentRouter = require('./routes/agent-routes.js');
 const queryRouter = require('./routes/Query-routes');
@@ -22,14 +24,15 @@ const callRouter = require('./routes/Call-routes');
 
 const app = express();
 
+// Set view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('trust proxy', true);
 
-
-
+// Morgan logging format
 const morganFormat = ":method :url :status :response-time ms";
 
+// Use morgan for logging HTTP requests
 app.use(
   morgan(morganFormat, {
       stream: {
@@ -45,13 +48,16 @@ app.use(
     },
   })
 );
+
+// Middleware setup
+app.use(helmet()); // Add helmet middleware for security
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-
+// Route handlers
 app.use('/agent', agentRouter); // For agent routes
 app.use('/lead', leadRouter); // For lead routes
 app.use('/query', queryRouter); // For query routes
@@ -60,19 +66,19 @@ app.use('/userRoles', userRolesRouter); // For user roles routes
 app.use('/disease', diseaseRouter); // For disease routes    
 app.use('/tags', tagsRouter); // For tags routes    
 app.use('/source', sourceRouter); // For source routes    
-app.use('/call', callRouter); // For call routes    
+app.use('/call', callRouter); // For call routes
 
-
+// Handle 404 errors
 app.use(function(req, res, next) {
   next(createError(404));
 });
-//error Handling
+
+// Error handling
 const ErrorHandler = require("./utils/errorHandler.js");
 const {generatedErrors} = require("./middlewares/errors.js");
 app.all("*", (req, res, next) => {
   next(new ErrorHandler(`Requested URL Not Found: ${req.url}`, 404));
 });
 app.use(generatedErrors);
-
 
 module.exports = app;
--- controllers/LeadControllers.js
+++ controllers/LeadControllers.js
@@ -1,168 +1,174 @@
 const { catchAsyncErrors } = require('../middlewares/catchAsyncErrors');
 const Leads = require('../Models/LeadsModel.js');
-const{leadQueue}= require("../utils/kylasLeadPipeline.js")
+const { leadQueue } = require("../utils/kylasLeadPipeline.js");
 const logger = require('../logger.js');
-const Agent = require("../Models/agentModel.js")
+const Agent = require("../Models/agentModel.js");
 
+// Create a new lead
 exports.createLead = catchAsyncErrors(async (req, res) => {
-
+    logger.info('Creating a new lead');
     const lastLead = await Leads.findOne().sort({ leadId: -1 }).exec();
-
-    let newLeadId = "K0-1000"; // Default starting ID
-
-    if (lastLead) {
-      const lastLeadIdNumber = parseInt(lastLead.leadId.split("-")[1]);
-      newLeadId = `K0-${lastLeadIdNumber + 1}`;
-    }
-
+    const newLeadId = lastLead ? `K0-${parseInt(lastLead.leadId.split("-")[1]) + 1}` : "K0-1000";
 
     const newLead = new Leads({
-      ...req.body,
-      leadId: newLeadId,
+        ...req.body,
+        leadId: newLeadId,
     });
 
-    // Save the lead
     await newLead.save();
+    logger.info(`Lead created successfully with ID: ${newLeadId}`);
 
     res.status(201).json({
-      message: "lead created successfully",
-      lead: newLead,
+        message: "Lead created successfully",
+        lead: newLead,
     });
-
 });
 
+// Update an existing lead
 exports.updateLead = catchAsyncErrors(async (req, res) => {
-  const { leadId } = req.params;
-  const updateData = req.body;
+    const { leadId } = req.params;
+    const updateData = req.body;
 
-  // Check if the updateData contains leadId - prevent updating it
-  if (updateData.leadId && updateData.leadId !== leadId) {
-    return res.status(400).json({ message: "LeadId cannot be updated." });
-  }
-
-  // Find the existing lead
-  const existingLead = await Leads.findOne({ leadId });
-
-  if (!existingLead) {
-    return res.status(404).json({ message: "Lead not found" });
-  }
-
-  // Find which fields are being updated
-  const updatedFields = {};
-  for (let key in updateData) {
-    if (key !== "leadId" && updateData[key] !== existingLead[key]) {
-      updatedFields[key] = updateData[key];
+    if (updateData.leadId && updateData.leadId !== leadId) {
+        logger.warn(`Attempt to update leadId from ${leadId} to ${updateData.leadId}`);
+        return res.status(400).json({ message: "LeadId cannot be updated." });
     }
-  }
-  const agent = await Agent.findById(req.user.id);
-  const IpAddress = req.ip
 
-  // Update the lead and add the changes to the `updatedData` field, using agentId
-  const updatedLead = await Leads.findOneAndUpdate(
-    { leadId },
-    {
-      $set: {  ...updateData, // Update the fields in the lead
-        LastUpdated_By: agent.agentId}, // Store the `agentId` of the updating agent},
-      $push: {
-        updatedData: {
-          updatedBy: agent.agentId, // Assuming req.user contains the agentId
-          updatedFields,
-          ipAddress:IpAddress,
-          updatedByEmail:agent.email,
-          updatedAt: Date.now(),
+    const [existingLead, agent] = await Promise.all([
+        Leads.findOne({ leadId }),
+        Agent.findById(req.user.id)
+    ]);
+
+    if (!existingLead) {
+        logger.warn(`Lead not found with ID: ${leadId}`);
+        return res.status(404).json({ message: "Lead not found" });
+    }
+
+    const updatedFields = {};
+    for (let key in updateData) {
+        if (key !== "leadId" && updateData[key] !== existingLead[key]) {
+            updatedFields[key] = updateData[key];
+        }
+    }
+
+    const updatedLead = await Leads.findOneAndUpdate(
+        { leadId },
+        {
+            $set: { ...updateData, LastUpdated_By: agent.agentId },
+            $push: {
+                updatedData: {
+                    updatedBy: agent.agentId,
+                    updatedFields,
+                    ipAddress: req.ip,
+                    updatedByEmail: agent.email,
+                    updatedAt: Date.now(),
+                },
+            },
         },
-      },
-    },
-    { new: true, runValidators: true }
-  );
+        { new: true, runValidators: true }
+    );
 
-  // If update was successful, return a success response with status code 200
-  if (updatedLead) {
-    return res.status(200).json({
-      success: true,
-      message: "Lead updated successfully",
-      data: updatedLead,
-    });
-  }
+    if (updatedLead) {
+        logger.info(`Lead updated successfully with ID: ${leadId}`);
+        return res.status(200).json({
+            success: true,
+            message: "Lead updated successfully",
+            data: updatedLead,
+        });
+    }
 });
 
-
+// Search for leads based on query parameters
 exports.searchLead = catchAsyncErrors(async (req, res) => {
-
+    logger.info('Searching for leads');
     const query = {};
 
-    // Loop through the query parameters and add them to the search query
     for (let key in req.query) {
-      if (req.query[key]) {
-        if (key === 'leadId' || key === 'firstName' || key === 'lastName' || key === 'address' || key==='leadOwner'|| key==='email'||key==='contact') {
-          query[key] = { $regex: req.query[key], $options: 'i' }; // Case-insensitive partial match
-        } else {
-          query[key] = req.query[key];
+        if (req.query[key]) {
+            query[key] = key === 'leadId' || key === 'firstName' || key === 'lastName' || key === 'address' || key === 'leadOwner' || key === 'email' || key === 'contact'
+                ? { $regex: req.query[key], $options: 'i' }
+                : req.query[key];
         }
-      }
     }
 
-    const lead = await Leads.find(query) // Exclude password field
-    res.json(lead);
-  
+    const leads = await Leads.find(query);
+    logger.info(`Found ${leads.length} leads matching the query`);
+    res.json(leads);
 });
 
-exports.allLeads = catchAsyncErrors(async(req,res)=>{
-  const { leadId } = req.params; // Destructure leadId from params
-  let allLeads;
+// Retrieve all leads with optional pagination
+exports.allLeads = catchAsyncErrors(async (req, res) => {
+    const { leadId } = req.params;
 
-  if (leadId) {
-    // If leadId is provided, find the specific lead
-    allLeads = await Leads.findOne({ leadId });
-    if (!allLeads) {
-      return res.status(404).json({ success: false, message: "Lead not found" });
+    if (leadId) {
+        logger.info(`Retrieving lead with ID: ${leadId}`);
+        const lead = await Leads.findOne({ leadId });
+        if (!lead) {
+            logger.warn(`Lead not found with ID: ${leadId}`);
+            return res.status(404).json({ success: false, message: "Lead not found" });
+        }
+        return res.status(200).json({ success: true, message: "Lead retrieved successfully", data: lead });
     }
-  } else {
-    // If no leadId, find all leads
-    allLeads = await Leads.find();
-    console.log(`Number of leads found: ${allLeads.length}`); // Log the number of leads
-  }
-  res.status(200).json({ success: true, message: "Leads retrieved successfully", allLeads });
-});
 
-exports.deleteLead = catchAsyncErrors(async (req, res) => {
-  const { leadId } = req.params;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 100;
+    const skip = (page - 1) * limit;
 
-  // Find the customer lead by leadId and delete it
-  const deletedLead = await Leads.findOneAndDelete({ leadId });
+    const [allLeads, totalLeads] = await Promise.all([
+        Leads.find().skip(skip).limit(limit),
+        Leads.countDocuments()
+    ]);
 
-  if (!deletedLead) {
-    return res.status(404).json({ message: "lead not found" });
-  }
-
-  res.json({ message: "lead deleted successfully" });
-});
-
-
-exports.kylasLead = catchAsyncErrors(async (req, res) => {
-  // Push the request to the queue
-  leadQueue.push({ req, res });
-});
-
-
-exports.interactLead = catchAsyncErrors(async (req, res) => {
-  try {
-    const newLeadData = req.body;
-    
-    console.log(`Interact Lead Data: ${JSON.stringify(newLeadData)}`);
-
+    logger.info(`Retrieved ${allLeads.length} leads (page ${page}, limit ${limit})`);
     res.status(200).json({
-      success: true,
-      message: "Lead created successfully",
-      data: newLeadData
+        success: true,
+        message: "Leads retrieved successfully",
+        total: totalLeads,
+        page,
+        limit,
+        data: allLeads,
     });
-  } catch (error) {
+});
 
-    logger.error(`Error processing lead: ${error.message}`);
-    res.status(500).json({
-      success: false,
-      message: "An error occurred while processing the lead."
-    });
-  }
-  
+// Delete a lead by lead ID
+exports.deleteLead = catchAsyncErrors(async (req, res) => {
+    const { leadId } = req.params;
+    logger.info(`Deleting lead with ID: ${leadId}`);
+
+    const deletedLead = await Leads.findOneAndDelete({ leadId });
+
+    if (!deletedLead) {
+        logger.warn(`Lead not found with ID: ${leadId}`);
+        return res.status(404).json({ message: "Lead not found" });
+    }
+
+    logger.info(`Lead deleted successfully with ID: ${leadId}`);
+    res.json({ message: "Lead deleted successfully" });
+});
+
+// Handle Kylas lead requests by pushing them to the queue
+exports.kylasLead = catchAsyncErrors(async (req, res) => {
+    logger.info('Handling Kylas lead request');
+    leadQueue.push({ req, res });
+});
+
+// Interact with a lead (for demonstration purposes)
+exports.interactLead = catchAsyncErrors(async (req, res) => {
+    try {
+        const newLeadData = req.body;
+
+        console.log(`Interact Lead Data: ${JSON.stringify(newLeadData)}`);
+
+        res.status(200).json({
+            success: true,
+            message: "Lead created successfully",
+            data: newLeadData
+        });
+    } catch (error) {
+        logger.error(`Error processing lead: ${error.message}`);
+        res.status(500).json({
+            success: false,
+            message: "An error occurred while processing the lead."
+        });
+    }
 });
