df8e35f357a028eace645479019c2e509aed5433
jayamjain@19 <jayamjain2626@gmail.com>
2024-09-25 12:57:59 +0530

    updateQueries

--- routes/Query-routes.js
+++ routes/Query-routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 var router = express.Router();
-const { queryCreation, getQuery, deleteQuery, updateQuery } = require('../controllers/queryControllers');
+const { queryCreation, getQuery, deleteQuery, updateQuery, searchQuery } = require('../controllers/queryControllers');
 
 // Create a new queries
 router.post('/create', queryCreation);
@@ -8,11 +8,14 @@ router.post('/create', queryCreation);
 //get queries
 router.get('/all', getQuery);
 
+router.get('/:queryId', searchQuery);
+
 //update queries
 router.put('/:queryId', updateQuery);
 
 //delete queries
 router.delete('/:queryId', deleteQuery);
+
 
 
 module.exports = router;
