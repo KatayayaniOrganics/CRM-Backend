794b8acc76c7fb6221870323a0e1af7038d7e931
Aman Kushwaha <amankushwaha1331@gmail.com>
2024-10-03 18:08:38 +0530

    Update index.js
    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./app'); 
 const logger = require('./logger');
 const port = process.env.PORT;
-const { server, io } = require('./socketapi');
+
 
 app.listen(port, () => {
   logger.info(`Server is running on http://localhost:${port}`);
