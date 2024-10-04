43d616f78028425650bc7b566150ffeaa907e35b
Bhumi Gupta <bhumi.gupta@example.com>
2024-10-04 16:49:01 +0530

    call string

--- Models/callsModel.js
+++ Models/callsModel.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const callSchema = new mongoose.Schema({
     callId: {type:String , unique:true, default:"CO-1001"},
-    query_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Query' },
-    customer_id: { type: mongoose.Schema.Types.ObjectId, ref: 'CustomerLead' },
-    agent_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Agent' },
+    query_id: { type: String, ref: 'Query' },
+    customer_id: { type: String, ref: 'CustomerLead' },
+    agent_id: { type: String, ref: 'Agent' },
     datetime: { type: Date, default: Date.now },
     duration: {
         type: Number,
