d5db5381b8e231be95fc42ecf2f96019299abc91
tyrant0407 <amankushwaha1331@gmail.com>
2024-10-05 11:51:03 +0530

    logsAdded

--- routes/Customer-routes.js
+++ routes/Customer-routes.js
@@ -2,17 +2,18 @@ const express = require('express');
 var router = express.Router();
 const { verifyToken } = require('../middlewares/authMiddleware');
 const {createCustomer,allCustomer,searchCustomer,deleteCustomer,updateCustomer} = require('../controllers/customerController');
+const { logRequest } = require('../middlewares/logDetails');
 
-router.post('/create',createCustomer); 
+router.post('/create',logRequest,createCustomer); 
 
-router.get('/all',allCustomer);
+router.get('/all',logRequest,allCustomer);
 
-router.get('/all/:customerId',allCustomer);
+router.get('/all/:customerId',logRequest,allCustomer);
 
-router.get('/search',searchCustomer);
+router.get('/search',logRequest,searchCustomer);
 
-router.delete('/delete/:customerId',deleteCustomer);
+router.delete('/delete/:customerId',logRequest,deleteCustomer);
 
-router.put('/update/:customerId',verifyToken,updateCustomer);
+router.put('/update/:customerId',logRequest,verifyToken,updateCustomer);
 
 module.exports = router;
--- routes/Disease-routes.js
+++ routes/Disease-routes.js
@@ -2,23 +2,23 @@ const express = require('express');
 const router = express.Router();
 const { createDisease, allDisease, searchDisease, updateDisease, deleteDisease } = require('../controllers/diseaseControllers');
 const { verifyToken } = require('../middlewares/authMiddleware');
-
+const { logRequest } = require('../middlewares/logDetails');
 // Create a new disease
-router.post('/create',createDisease);
+router.post('/create',logRequest,createDisease);
 
 //all Disease
-router.get('/all',allDisease);
+router.get('/all',logRequest,allDisease);
 
 //all Disease by id
-router.get('/all/:diseaseId',allDisease);
+router.get('/all/:diseaseId',logRequest,allDisease);
 
 //Search disease
-router.get('/search',searchDisease);
+router.get('/search',logRequest,searchDisease);
 
 //Update Disease
-router.put("/:diseaseId",verifyToken,updateDisease);
+router.put("/:diseaseId",logRequest,verifyToken,updateDisease);
 
 //Delete Disease
-router.delete("/:diseaseId", deleteDisease);
+router.delete("/:diseaseId",logRequest, deleteDisease);
 
 module.exports = router;
